060c28de35bb9a71e1c48edbb714f51e15d04e26
LocNgoXuan23 <>
2022-01-02 21:16:19 +0700

    added few fixes

--- server.js
+++ server.js
@@ -30,7 +30,6 @@ const errorHandlerMiddleware = require('./middleware/error-handler')
 app.use(morgan('tiny'))
 app.use(express.json())
 app.use(cookieParser(process.env.JWT_SECRET))
-app.use(express.static('./public'))
 app.use(cors())
 
 app.use('/api/v1/auth', authRouter)
