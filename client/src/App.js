4afdd5876a8d5e13df937c165bdaa6837b88fffb
LocNgoXuan23 <>
2022-01-02 18:51:50 +0700

    added few fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,85 +1,77 @@
-import { useState } from 'react';
-const rootUrl = 'http://localhost:5000';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Navbar, Sidebar, Footer } from './components'
 
-function App() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+import { Home, Product, ProductBackend, SingleProduct, About, Readme, Cart, CartHistory, Error, Login, Checkout, Register, ChangePassword, AuthWrapper, PrivateRoute, AdminRoute, Admin, AdminUser, AdminProduct, AdminCart, AdminSingleCart } from './pages'
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (!email || !password) return;
-    const user = { email, password };
-
-    try {
-      // const url = `${rootUrl}/api/v1/auth/login`;
-      const url = `/api/v1/auth/login`;
-      await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-type': 'application/json',
-        },
-        body: JSON.stringify(user),
-      });
-      setPassword('');
-      setEmail('');
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const fetchTesting = async () => {
-    // const url = `${rootUrl}/api/v1`;
-    const url = `/api/v1`;
-    await fetch(url);
-  };
-  const fetchLogout = async () => {
-    // const url = `${rootUrl}/api/v1/auth/logout`;
-    const url = `/api/v1/auth/logout`;
-    await fetch(url);
-  };
-
+function App() {  
   return (
-    <>
-      <form className='form' onSubmit={handleSubmit}>
-        <h4>login form</h4>
-        <div className='form-row'>
-          <label htmlFor='email' className='form-label'>
-            Email
-          </label>
-          <input
-            type='email'
-            className='form-input email-input'
-            name='email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className='form-row'>
-          <label htmlFor='password' className='form-label'>
-            Password
-          </label>
-          <input
-            type='password'
-            name='password'
-            className='form-input password-input'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type='submit' className='btn btn-block submit-btn'>
-          submit
-        </button>
-      </form>
-      <div className='container'>
-        <button className='btn testing-btn' onClick={fetchTesting}>
-          Testing
-        </button>
-        <button className='btn logout-btn' onClick={fetchLogout}>
-          Logout
-        </button>
-      </div>
-    </>
-  );
+    <AuthWrapper>
+      <Router>
+        <Navbar />
+        <Sidebar />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route exact path='/about'>
+            <About />
+          </Route>
+          <Route exact path='/readme'>
+            <Readme />
+          </Route>
+          <Route exact path='/cart'>
+            <Cart />
+          </Route>
+          <PrivateRoute exact path='/cart/history'>
+            <CartHistory />
+          </PrivateRoute>
+          <Route exact path='/products'>
+            <Product />
+          </Route>
+          <Route exact path='/productsBackend'>
+            <ProductBackend />
+          </Route>
+          <Route exact path="/products/:id" children={<SingleProduct />} />
+
+          {/* Auth */}
+          <Route exact path='/login'>
+            <Login />
+          </Route>
+          <Route exact path='/register'>
+            <Register />
+          </Route>
+          <PrivateRoute exact path='/changePassword'>
+            <ChangePassword />
+          </PrivateRoute>
+          <PrivateRoute exact path='/checkout'>
+            <Checkout />
+          </PrivateRoute>
+
+          {/* admin */}
+          <AdminRoute exact path='/admin'>
+            <Admin />
+          </AdminRoute>
+          <AdminRoute exact path='/admin/user'>
+            <AdminUser />
+          </AdminRoute>
+          <AdminRoute exact path='/admin/product'>
+            <AdminProduct />
+          </AdminRoute>
+          <AdminRoute exact path='/admin/cart'>
+            <AdminCart />
+          </AdminRoute>
+          <AdminRoute exact path="/carts/:id" children={<AdminSingleCart />} />
+
+
+          <Route path="*">
+            <Error />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </AuthWrapper>
+  )
 }
 
-export default App;
+export default App
